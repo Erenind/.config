88a1d83f9f2b9ef906a9511e515201c9ae3c0e33
kyee <chaojianbili@outlook.com>
2026-07-15 16:16:20 +0800

    kitty & hyprland

--- hypr/kk/windowRule.lua
+++ hypr/kk/windowRule.lua
@@ -19,5 +19,25 @@ hl.window_rule({
 
 hl.window_rule({
     match = {class = "terraria"},
-    immediate = true;
+    immediate = true
+})
+
+hl.window_rule({
+    match = {
+        class = "waypaper"
+    },
+    float = true
+})
+
+
+local obsidianWidth = 1920 * 0.7
+local obsidianHeight = obsidianWidth * 0.618
+
+
+hl.window_rule({
+    match = {
+        class = "obsidian"
+    },
+    float = true,
+    size = {tostring(obsidianWidth),tostring(obsidianHeight)}
 })
